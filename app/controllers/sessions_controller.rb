3f33d325323e0869e3a11cbc462c85cfea597259
Kari Bancroft <kariabancroft@gmail.com>
2015-07-13 14:05:06 -0700

    Add users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to dashboard_path(@user)
+      redirect_to home_path
     else
       flash.now[:error] = "Try again!"
       render 'new'
